7340e1537a5d5d79a544795f70d0cd7a85494d99
Jeff Woltjen (he/him) <Jeff.Woltjen@gmail.com>
2022-01-04 12:51:50 -0600

    create question-controller class

--- quiz_controller.js
+++ quiz_controller.js
@@ -5,4 +5,19 @@ class Quiz {
         this.score = 0; 
         this.questionIndex = 0; 
     }
+
+    getQuestionIndex(){
+        return this.questions[this.questionIndex]; 
+    }
+    isEnded(){
+        return this.questions.length === this.questionIndex; 
+    }
+    guess(answer){
+        this.questionIndex++; 
+
+        if(this.getQuestionIndex().correctAnswer(answer)){
+            this.score++
+        }
+    }
 }
+
